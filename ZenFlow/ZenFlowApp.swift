38e6ead025366f77d8ba4335ed85575e8518128f
TunamiRider <yoite.sy@gmail.com>
2026-05-21 11:24:20 -0700

    Initial Commit

---
+++ ZenFlow/ZenFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZenFlowApp.swift
+//  ZenFlow
+//
+//  Created by Yuki Suzuki on 5/21/26.
+//
+
+import SwiftUI
+
+@main
+struct ZenFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
